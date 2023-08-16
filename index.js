349eb36faabbd6b121c992dc297c0c24b0bbaf0d
rishbh2168 <98745068+rishbh2168@users.noreply.github.com>
2023-08-16 23:25:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//alert("working!");
+var randomNumber1 = Math.floor(Math.random()*6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice1.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Rishabh win :)";
+}
+
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Rishi win :)";
+
+}
+
+else{
+    document.querySelector("h1").innerHTML = "TRY AGAIN ;)";
+
+}
