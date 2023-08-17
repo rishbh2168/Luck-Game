0a0737465c2bf195e4f41c5d7c05370fd0175e0a
rishbh2168 <98745068+rishbh2168@users.noreply.github.com>
2023-08-17 14:47:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var randomNumber1 = Math.floor(Math.random()*6) + 1; //1-6
 
 var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice1.png
+var randomImageSource = randomDiceImage; //images/dice1.png - images/dice1.png
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -13,7 +13,7 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 
-var randomImageSource2 = "images/" + randomDiceImage2;
+var randomImageSource2 = randomDiceImage2;
 
 var image2 = document.querySelectorAll("img")[1];
 
